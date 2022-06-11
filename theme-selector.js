4929626ea98a80d09c4ac2ff5b12ee5e53b6e2a1
Russell Beswick <russell@bes.works>
2022-06-11 13:48:38 -0400

    fix #1 by moving prefersDark out of loadTheme

--- theme-selector.js
+++ theme-selector.js
@@ -1,5 +1,8 @@
 const debug = (false) ? console : null;
 
+let prefersDark = matchMedia('(prefers-color-scheme: dark)');
+prefersDark.addEventListener('change', event => loadTheme());
+
 function setTheme(theme) {
   if (theme == 'auto') {
     try { localStorage.removeItem('theme'); }
@@ -17,8 +20,6 @@ function getTheme() {
 }
 
 function loadTheme(theme) {
-  let prefersDark = matchMedia('(prefers-color-scheme: dark)');
-  prefersDark.addEventListener('change', event => loadTheme());
   try { theme = localStorage.getItem('theme'); }
   catch (err) { debug?.warn('unable to load theme'); }
   theme ??= (prefersDark.matches) ? 'dark' : 'light';
